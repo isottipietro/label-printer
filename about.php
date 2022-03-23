260470835f164adb8805555c9c9ad0f058217d06
isottipietro <86808453+isottipietro@users.noreply.github.com>
2022-03-23 22:16:49 +0100

    0.21 added about section

--- about.php
+++ about.php
@@ -1,2 +1,10 @@
 <h2>Istruzioni</h2>
 <p>Non c'è niente qui, devo ancora scriverle...</p>
+<button type="button" class="collapsible" id="defaultOpen">Cosa c'è scritto nelle etichette?</button>
+<div class="content">
+  <p>Lorem ipsum...</p>
+</div>
+<button type="button" class="collapsible">Hai altre domande?</button>
+<div class="content">
+  <p>Lorem ipsum...</p>
+</div>
